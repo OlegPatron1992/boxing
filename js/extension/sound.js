964003e76ab09a181c7521e82dd91670cff00e91
Oleg Patrushev <olegpatron1992@gmail.com>
2018-03-25 18:17:23 +0300

    dev

--- js/extension/sound.js
+++ js/extension/sound.js
@@ -1,8 +1,16 @@
 game.sound = (function (that) {
     'use strict';
 
-    that._sounds = {};
+    that._soundsUrl = {
+        'attack-base': './audio/attack_base.mp3',
+        'attack-block': './audio/attack_block.mp3',
+        'attack-dead': './audio/attack_dead.mp3',
+        'attack-move': './audio/attack_move.mp3',
+        'ring-bell': './audio/ring_bell.mp3'
+    };
+    that._soundsBuffer = {};
     that._enabled = true;
+    that._context = new AudioContext();
 
     that.setEnabled = function (enabled) {
         that._enabled = enabled;
@@ -11,32 +19,42 @@ game.sound = (function (that) {
         return that._enabled;
     };
 
-    that._registerSound = function (soundName) {
-        that._sounds[soundName] = document.getElementById(game.prefix + '-sound-' + soundName);
+    that._loadSoundFile = function (soundName) {
+        var xhr;
+
+        xhr = new XMLHttpRequest();
+        xhr.open('GET', that._soundsUrl[soundName], true);
+        xhr.responseType = 'arraybuffer';
+        xhr.onload = function (e) {
+            that._context.decodeAudioData(xhr.response, function (buffer) {
+                that._soundsBuffer[soundName] = buffer;
+            }, function (e) {
+                alert('Error decoding file', e);
+            });
+        };
+        xhr.send();
     };
 
     that.configure = function () {
-        [
-            'attack-base',
-            'attack-block',
-            'attack-dead',
-            'attack-move',
-            'ring-bell'
-        ].forEach(function (sceneName) {
-            that._registerSound(sceneName);
-        });
-    };
+        var soundName;
 
-    that.play = function (soundName) {
-        if (that._enabled) {
-            that._sounds[soundName].muted = false;
-            that._sounds[soundName].currentTime = 0;
-            that._sounds[soundName].play();
+        for (soundName in that._soundsUrl) {
+            if (that._soundsUrl.hasOwnProperty(soundName)) {
+                that._loadSoundFile(soundName);
+            }
         }
     };
 
-    that.stop = function (soundName) {
-        that._sounds[soundName].pause();
+    that.play = function (soundName) {
+        var source;
+
+        if (that._enabled) {
+            source = that._context.createBufferSource();
+            source.buffer = that._soundsBuffer[soundName];
+            source.loop = false;
+            source.connect(that._context.destination);
+            source.start(0);
+        }
     };
 
     return that;
